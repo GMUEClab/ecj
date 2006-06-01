f65744bb299bb1d8c1b8d407793d15a069794895
feijai <feijai@1c647de6-f618-7a77-627f-ce647b82753d>
2006-06-01 00:41:34 +0000

    Moved Teambots Issue number: Obtained from: Submitted by: Reviewed by:

---
+++ trunk/contrib/teambots/ec/teambots/EvolvedControlSystem.java
@@ -0,0 +1,21 @@
+/*
+  Copyright 2006 by Sean Luke and George Mason University
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+
+package ec.teambots;
+
+import EDU.gatech.cc.is.abstractrobot.*;
+
+/**
+ * This is the superclass for all evolved robot Control Systems.
+ *
+ * Created: Thu Mar 15 15:28:30 2001
+ * By: Liviu Panait
+ */
+
+public class EvolvedControlSystem extends ControlSystemS
+    {
+    }
