c2fca0e489b08160ba5b7755cd421cafc581a778
feijai <feijai@1c647de6-f618-7a77-627f-ce647b82753d>
2008-01-05 21:24:30 +0000

    Yo Issue number: Obtained from: Submitted by: Reviewed by:

--- trunk/ecj/ec/eval/MasterProblem.java
+++ trunk/ecj/ec/eval/MasterProblem.java
@@ -39,7 +39,7 @@ import java.io.*;
  */
 
 public class MasterProblem extends Problem implements SimpleProblemForm, GroupedProblemForm 
-{
+    {
 
     public static final String P_DEBUG_INFO = "debug-info";
     public static final String P_CHUNK_SIZE = "chunk-size";
@@ -57,25 +57,25 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
 
     // except for the problem, everything else is shallow-cloned
     public Object clone()
-    {
+        {
         MasterProblem c = (MasterProblem)(super.clone());
 
         // shallow-cloned stuff
         c.server = server;
         c.serverThread = serverThread;
         c.batchMode = batchMode;
-	c.chunkSize = chunkSize; 
-	c.showDebugInfo = showDebugInfo;
+        c.chunkSize = chunkSize; 
+        c.showDebugInfo = showDebugInfo;
 
         // deep-cloned stuff
         c.problem = (Problem)(problem.clone());
 
         return c;
-    }
+        }
 
     // setup
     public void setup(final EvolutionState state, final Parameter base) 
-    {
+        {
         Thread.currentThread().setName("MainThread: ");
         super.setup(state, base);
         showDebugInfo = state.parameters.getBoolean(base.push(P_DEBUG_INFO),null,false);
@@ -85,18 +85,18 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
             state.output.fatal("The chunk size must be an integer > 0.", base.push(P_CHUNK_SIZE));
 
         batchMode = false;
-    }
+        }
 
     // prepare for a batch of evaluations
     public void prepareToEvaluate(final EvolutionState state, final int threadnum)
-    {
+        {
         if (chunkSize > 1) queue = new ArrayList();
         batchMode = true;
-    }
+        }
 
     // wait until a batch of evaluations is finished
     public void finishEvaluating(final EvolutionState state, final int threadnum)
-    {
+        {
         if(showDebugInfo)
             state.output.message(Thread.currentThread().getName() + "Waiting for all slaves to finish.");
         flush(state, threadnum);
@@ -106,41 +106,41 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
         batchMode = false;
         if(showDebugInfo)
             state.output.message(Thread.currentThread().getName() + "All slaves have finished their jobs.");
-    }
+        }
 
     // evaluate a regular individual
     public void evaluate(EvolutionState state, Individual ind, int threadnum)
-    {
+        {
         if (chunkSize > 1 && batchMode == true)    // chunked evaluation mechanism
             {
-                queue.add(ind);
-                if (queue.size() >= chunkSize)
-                    flush(state, threadnum);
+            queue.add(ind);
+            if (queue.size() >= chunkSize)
+                flush(state, threadnum);
             }
         else    /// ordinary evaluation mechanism  
             evaluate(state, new Individual[] { ind }, threadnum);           
-    }
+        }
         
         
     ArrayList queue;
     void flush(EvolutionState state, int threadnum)
-    {
+        {
         if (queue!=null && queue.size() > 0 )
             {
-                Individual[] inds = new Individual[queue.size()];
-                for(int i = 0; i < queue.size(); i++)
-                    {
-                        inds[i] = (Individual)(queue.get(i));
-                    }
-                evaluate(state, inds, threadnum);
+            Individual[] inds = new Individual[queue.size()];
+            for(int i = 0; i < queue.size(); i++)
+                {
+                inds[i] = (Individual)(queue.get(i));
+                }
+            evaluate(state, inds, threadnum);
             }
         queue = new ArrayList();
-    }
+        }
 
 
     // send a group of individuals to one slave for evaluation 
     void evaluate(EvolutionState state, Individual inds[], int threadnum)
-    {
+        {
         if(showDebugInfo)
             state.output.message(Thread.currentThread().getName() + "Starting an evaluation.");
                 
@@ -149,11 +149,11 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
         boolean found = false;
         for (int x=0;x<state.population.subpops.length && !found;x++)
             {
-                if (state.population.subpops[x].species == inds[0].species)
-                    {
-                        subPopNum = x;
-                        found = true;
-                    }
+            if (state.population.subpops[x].species == inds[0].species)
+                {
+                subPopNum = x;
+                found = true;
+                }
             }
                 
         if (!found)
@@ -175,7 +175,7 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
         if( !batchMode )
             server.slaveMonitor.waitForAllSlavesToFinishEvaluating( state );
         if(showDebugInfo) state.output.message(Thread.currentThread().getName() + "Finished evaluating the individual.");
-    }
+        }
         
         
         
@@ -185,45 +185,45 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
      */
     public void describe(Individual ind, EvolutionState state, int threadnum,
                          int log, int verbosity) 
-    {
+        {
         if (!(problem instanceof SimpleProblemForm)) 
             {
-                state.output.fatal("StarProblem.describe(...) invoked, but the Problem is not of SimpleProblemForm");
+            state.output.fatal("StarProblem.describe(...) invoked, but the Problem is not of SimpleProblemForm");
             }
                 
         ((SimpleProblemForm)problem).describe( ind, state, threadnum, log, verbosity);
-    }
+        }
 
     /* (non-Javadoc)
      * @see ec.coevolve.GroupedProblemForm#preprocessPopulation(ec.EvolutionState, ec.Population)
      */
     public void preprocessPopulation(EvolutionState state, Population pop) 
-    {
+        {
         if (!(problem instanceof GroupedProblemForm)) 
             {
-                state.output.fatal("StarProblem.preprocessPopulation(...) invoked, but the Problem is not of GroupedProblemForm");
+            state.output.fatal("StarProblem.preprocessPopulation(...) invoked, but the Problem is not of GroupedProblemForm");
             }
                 
         ((GroupedProblemForm) problem).preprocessPopulation(state, pop);
-    }
+        }
 
     /* (non-Javadoc)
      * @see ec.coevolve.GroupedProblemForm#postprocessPopulation(ec.EvolutionState, ec.Population)
      */
     public void postprocessPopulation(EvolutionState state, Population pop) 
-    {
+        {
         if (!(problem instanceof GroupedProblemForm)) 
             {
-                state.output.fatal("StarProblem.postprocessPopulation(...) invoked, but the Problem is not of GroupedProblemForm");
+            state.output.fatal("StarProblem.postprocessPopulation(...) invoked, but the Problem is not of GroupedProblemForm");
             }
                 
         ((GroupedProblemForm) problem).postprocessPopulation(state, pop);
-    }
+        }
 
     // regular coevolutionary evaluation
     public void evaluate(EvolutionState state, Individual[] inds,
                          boolean[] updateFitness, boolean countVictoriesOnly, int threadnum)
-    {
+        {
         if(showDebugInfo)
             state.output.message("Starting a coevolutionary evaluation.");
 
@@ -232,21 +232,21 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
         boolean subPopNumFound = false;
         for(int i=0;i<inds.length;i++)
             {
-                subPopNumFound = false;
-                for (int x=0;x<state.population.subpops.length && !subPopNumFound;x++)
+            subPopNumFound = false;
+            for (int x=0;x<state.population.subpops.length && !subPopNumFound;x++)
+                {
+                if (state.population.subpops[x].species == inds[i].species)
                     {
-                        if (state.population.subpops[x].species == inds[i].species)
-                            {
-                                subPopNum[i] = x;
-                                subPopNumFound = true;
-                                break;
-                            }
+                    subPopNum[i] = x;
+                    subPopNumFound = true;
+                    break;
                     }
-                if (!subPopNumFound)
-                    {
-                        // Is it possible that there isn't a matching species?
-                        state.output.fatal("Whoa!  Couldn't find a matching species for Individual!");
-                    }
+                }
+            if (!subPopNumFound)
+                {
+                // Is it possible that there isn't a matching species?
+                state.output.fatal("Whoa!  Couldn't find a matching species for Individual!");
+                }
             }
 
         // Acquire a slave socket
@@ -267,51 +267,51 @@ public class MasterProblem extends Problem implements SimpleProblemForm, Grouped
 
         if(showDebugInfo)
             state.output.message("Finished the coevolutionary evaluation.");
-    }
+        }
 
     /** Custom serialization */
     private void writeObject(ObjectOutputStream out) throws IOException
-    {
+        {
         out.writeObject(problem);
-    }
+        }
 
     /** Custom serialization */
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
-    {
+        {
         problem = (Problem) in.readObject();
-    }
+        }
 
     /** Initialize contacts with the slaves */
     public void initializeContacts( final EvolutionState state )
-    {
+        {
         server = new MasterProblemServer(showDebugInfo);
         server.setupServerFromDatabase(state);
         if(showDebugInfo)
             state.output.message(Thread.currentThread().getName() + "Spawning the server thread.");
         serverThread = server.spawnThread();
-    }
+        }
 
     /** Reinitialize contacts with the slaves */
     public void reinitializeContacts( final EvolutionState state )
-    {
+        {
         initializeContacts(state);
-    }
+        }
 
     /** Gracefully close contacts with the slaves */
     public void closeContacts(EvolutionState state, int result)
-    {
+        {
         this.server.shutdown();
         try
             {
-                this.serverThread.join();
+            this.serverThread.join();
             }
         catch (InterruptedException e)
             {
             }
-    }
+        }
         
     public boolean canEvaluate() 
-    {
+        {
         return (server.slaveMonitor.numAvailableSlaves() != 0); 
+        }
     }
-}
