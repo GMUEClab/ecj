299fe0ac6448ebb5aa3ddec86f347135372cb140
gbalan <gbalan@1c647de6-f618-7a77-627f-ce647b82753d>
2009-02-14 03:15:11 +0000

    *** empty log message ***

---
+++ trunk/ecj/ec/app/moobenchmarks/ZDT3.java
@@ -0,0 +1,84 @@
+package ec.app.moobenchmarks;
+
+import ec.*;
+import ec.util.*;
+import ec.vector.*;
+import ec.simple.*;
+import ec.multiobjective.*;
+
+/**
+ * 
+ * ZDT-T3  The Pareto-optimal front is discrete, it consists
+ * of several noncontiguous convex parts.
+ * 
+ * <p>The Pareto-optimal front is formed with g(X) = 1. There is discontinuity
+ * in the front (due to sin) but no discontinuity in the parameter space.
+ * 
+ * <p>Zitzler, E., Deb, K., and Thiele, L., 2000, Comparison of Multiobjective Evolutionary
+ * Algorithms: Empirical Results, Evolutionary Computation, Vol. 8, No. 2, pp173-195.
+ * 
+ * <p><b>Parameters</b><br>
+ * <table>
+ * <tr><td valign=top><i>base</i>.<tt>numvariables</tt><br>
+ * <font size=-1>int (default=30)</font></td>
+ * <td valign=top>The number of variables; genome-size is set to this value internally.</td></tr>
+ * </table>
+ * 
+ * @author  Gabriel Balan
+ */
+public class ZDT3 extends Problem implements SimpleProblemForm
+{
+	public int numDecisionVars;
+	public static final String P_NUMVARS = "numvariables"; 
+
+
+    public void setup(final EvolutionState state, final Parameter base) 
+	{
+		super.setup(state, base);
+		Parameter def = defaultBase();		
+		numDecisionVars = state.parameters.getIntWithDefault(base.push(P_NUMVARS), def.push(P_NUMVARS),30);
+		if (numDecisionVars< 2)
+			state.output.fatal("The number of variables must be an integer > 1", base.push(P_NUMVARS), def.push(P_NUMVARS));
+
+
+		/**
+		 * I want to force the following values:
+		 * 
+		 * pop.subpop.0.species = ec.vector.FloatVectorSpecies
+		 * pop.subpop.0.species.ind = ec.vector.FloatVectorIndividual
+		 * pop.subpop.0.species.min-gene = 0
+		 * pop.subpop.0.species.max-gene = 1
+		 * pop.subpop.0.species.genome-size = numDecisionVars //could be anything, should be 30
+		 */
+		state.parameters.set(new Parameter("pop.subpop.0.species"),"ec.vector.FloatVectorSpecies");
+		state.parameters.set(new Parameter("pop.subpop.0.species.ind"),"ec.vector.FloatVectorIndividual");
+		state.parameters.set(new Parameter("pop.subpop.0.species.genome-size"),""+numDecisionVars);
+		state.parameters.set(new Parameter("pop.subpop.0.species.min-gene"),"0");
+		state.parameters.set(new Parameter("pop.subpop.0.species.max-gene"),"1");
+		state.parameters.set(new Parameter("pop.subpop.0.species.fitness.numobjectives"),"2");
+	}
+	private static final double TEN_PI = Math.PI*10;
+	public void evaluate(EvolutionState state, Individual ind, int subpopulation, int threadnum)
+	{
+		if(ind.evaluated)
+			return;
+		float[] genome = ((FloatVectorIndividual)ind).genome;
+		float[] fitnesses = ((MultiObjectiveFitness)ind.fitness).multifitness;
+		float f = fitnesses[0] = genome[0];
+		float sum = 0;
+		for(int i = 1; i< numDecisionVars; ++i)
+			sum += genome[i];
+		double g = 1+9*sum/(numDecisionVars-1);
+		double foverg = f/g;
+		double h = 1-Math.sqrt(foverg) - foverg * Math.sin(TEN_PI * f);
+		fitnesses[1] = (float)(g*h);
+		ind.evaluated = true;
+	}
+    public void describe(final Individual ind, 
+            final EvolutionState state, 
+            final int subpopulation,
+            final int threadnum,
+            final int log,
+            final int verbosity)
+    {}	
+}
---
+++ trunk/ecj/ec/app/moobenchmarks/ZDT4.java
@@ -0,0 +1,89 @@
+package ec.app.moobenchmarks;
+
+import ec.*;
+import ec.util.*;
+import ec.vector.*;
+import ec.simple.*;
+import ec.multiobjective.*;
+
+/**
+ * ZDT-T4  contains local Pareto-optimal fronts => tests ability to deal 
+ * with multimodality.
+ * 
+ * <p>The global Pareto-optimal front is formed with g(X) = 1;
+ * The best local Pareto-optimal front with g(X) = 1.25;
+ * Note that not all local pareto-optimal sets are distinguishable in
+ * the obejctive space.
+ * 
+ * <p>Zitzler, E., Deb, K., and Thiele, L., 2000, Comparison of Multiobjective Evolutionary
+ * Algorithms: Empirical Results, Evolutionary Computation, Vol. 8, No. 2, pp173-195.
+ * 
+ * <p><b>Parameters</b><br>
+ * <table>
+ * <tr><td valign=top><i>base</i>.<tt>numvariables</tt><br>
+ * <font size=-1>int (default=10)</font></td>
+ * <td valign=top>The number of variables; genome-size is set to this value internally.</td></tr>
+ * </table>
+ * 
+ * @author  Gabriel Balan
+ */
+public class ZDT4 extends Problem implements SimpleProblemForm
+{
+	public int numDecisionVars;
+	public static final String P_NUMVARS = "numvariables"; 
+
+
+    public void setup(final EvolutionState state, final Parameter base) 
+	{
+		super.setup(state, base);
+		Parameter def = defaultBase();
+		numDecisionVars = state.parameters.getIntWithDefault(base.push(P_NUMVARS), def.push(P_NUMVARS),10);
+		if (numDecisionVars< 2)
+			state.output.fatal("The number of variables must be an integer > 1", base.push(P_NUMVARS), def.push(P_NUMVARS));
+
+
+		/**
+		 * I want to force the following values:
+		 * 
+		 * pop.subpop.0.species = ec.vector.FloatVectorSpecies
+		 * pop.subpop.0.species.ind = ec.vector.FloatVectorIndividual
+		 * pop.subpop.0.species.min-gene.0 = 0
+		 * pop.subpop.0.species.max-gene.0 = 1
+		 * pop.subpop.0.species.min-gene = -5
+		 * pop.subpop.0.species.max-gene = 5
+		 * pop.subpop.0.species.genome-size = numDecisionVars //could be anything, should be 10
+		 */
+		state.parameters.set(new Parameter("pop.subpop.0.species"),"ec.vector.FloatVectorSpecies");
+		state.parameters.set(new Parameter("pop.subpop.0.species.ind"),"ec.vector.FloatVectorIndividual");
+		state.parameters.set(new Parameter("pop.subpop.0.species.genome-size"),""+numDecisionVars);
+		state.parameters.set(new Parameter("pop.subpop.0.species.min-gene.0"),"0");
+		state.parameters.set(new Parameter("pop.subpop.0.species.max-gene.0"),"1");
+		state.parameters.set(new Parameter("pop.subpop.0.species.min-gene"),"-5");
+		state.parameters.set(new Parameter("pop.subpop.0.species.max-gene"),"5");
+		state.parameters.set(new Parameter("pop.subpop.0.species.fitness.numobjectives"),"2");
+	}
+	private static final double FOUR_PI = Math.PI*4;
+	public void evaluate(EvolutionState state, Individual ind, int subpopulation, int threadnum)
+	{
+		if(ind.evaluated)
+			return;
+		float[] genome = ((FloatVectorIndividual)ind).genome;
+		float[] fitnesses = ((MultiObjectiveFitness)ind.fitness).multifitness;
+		float f = fitnesses[0] = genome[0];
+		float sum = 0;
+		for(int i = 1; i< numDecisionVars; ++i)
+			sum += genome[i]*genome[i]- 10*Math.cos(FOUR_PI * genome[i]);
+			
+		double g = 1+10*(numDecisionVars-1)+sum;
+		double h = 1-Math.sqrt(f/g);
+		fitnesses[1] = (float)(g*h);
+		ind.evaluated = true;
+	}
+    public void describe(final Individual ind, 
+            final EvolutionState state, 
+            final int subpopulation,
+            final int threadnum,
+            final int log,
+            final int verbosity)
+    {}	
+}
