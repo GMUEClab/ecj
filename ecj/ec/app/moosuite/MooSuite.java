be1dc5b246432997036000db2cf44e9adf0d3f54
gbalan <gbalan@1c647de6-f618-7a77-627f-ce647b82753d>
2009-04-08 22:35:22 +0000

    *** empty log message ***

--- ecj/ec/app/moosuite/MooSuite.java
+++ ecj/ec/app/moosuite/MooSuite.java
@@ -16,27 +16,21 @@ import ec.vector.*;
 /**
    Several standard Multi-objective benchmarks are implemented: 
    <ul>
-   <li>ZDT1: covex Pareto front, formed with g(X) = 1. 
-   <li>ZDT2, 
-   <li>ZDT3, 
-   <li>ZDT4, 
-   <li>ZDT6, 
-   <li>SPHERE (aka SCH). 
-   </ul>
-   
-   <p>[ZDT]: Zitzler, E., Deb, K., and Thiele, L., 2000, Comparison of Multiobjective Evolutionary
-   Algorithms: Empirical Results, Evolutionary Computation, Vol. 8, No. 2, pp173-195.
-   
-   <p>Schaffer, J. D. (1985).Multiple objective optimization with vector evaluated genetic
-   algorithms. In J. J. Grefenstette (Ed.), Proceedings of an International Conference
-   on Genetic Algorithms and Their Applications, Pittsburgh, PA, pp. 93-100.
-   sponsored by Texas Instruments and U.S. Navy Center for Applied Research in
-   Artificial Intelligence (NCARAI).
-   
-   <p>Laumanns, M., G. Rudolph, and H.-P. Schwefel (2001, June). Mutation control
-   and convergence in evolutionary multi-objective optimization. In Proceedings of
-   the 7th International Mendel Conference on Soft Computing (MENDEL 2001), Brno, Czech Republic.
-   
+   <li>ZDT1: Zitzler, Deb & Thiele
+   <li>ZDT2: Zitzler, Deb & Thiele 
+   <li>ZDT3: Zitzler, Deb & Thiele 
+   <li>ZDT4: Zitzler, Deb & Thiele 
+   <li>ZDT6: Zitzler, Deb & Thiele 
+   <li>SPHERE: ftp.tik.ee.ethz.ch/pub/people/zitzler/ZLT2001a.pdf 
+   <li>SCH: (Schaffer), (a.k.a. F1 in Srinivas & Deb)
+   <li>F2: (Schaffer), (Srinivas & Deb),  (Coello Coello & Cortes)
+   <li>unconstrained F3: Schaffer, Srinivas & Deb  (Chankong & Haimes)
+   <li>QV: Quagliarella & Vicini
+   <li>FON: Fonseca & Fleming
+   <li>POL: Poloni
+	   <li>KUR: Kursawe from the Errata of Zitzler's TIK-Report 103: "SPEA2: Improving the Strength Pareto Evolutionary Algorithm"
+	    (note that many different versions are described in the literature).
+   </ul>   
 
    <p><b>Parameters</b><br>
    <table>
@@ -64,7 +58,13 @@ public class MooSuite extends Problem implements SimpleProblemForm
     public static final String P_ZDT6 = "zdt6";
     public static final String P_SPHERE = "sphere";
     public static final String P_SCH = "sch";
-
+    public static final String P_FON = "fon";
+    public static final String P_QV = "qv";
+    public static final String P_POL = "pol";
+    public static final String P_KUR = "kur";
+    public static final String P_F1 = "f1";    
+    public static final String P_F2 = "f2";
+    public static final String P_F3 = "unconstrained-f3";
 
     public static final int PROB_SPHERE = 0;
     public static final int PROB_ZDT1 = 1;
@@ -72,6 +72,13 @@ public class MooSuite extends Problem implements SimpleProblemForm
     public static final int PROB_ZDT3 = 3;
     public static final int PROB_ZDT4 = 4;
     public static final int PROB_ZDT6 = 6;
+    public static final int PROB_FON = 7;
+    public static final int PROB_POL = 8;
+    public static final int PROB_KUR = 9;
+    public static final int PROB_QV = 10;
+    public static final int PROB_SCH = 11;
+    public static final int PROB_F2 = 12;
+    public static final int PROB_F3 = 13;
 
     public int problemType = PROB_ZDT1;  // defaults on zdt1
 
@@ -89,8 +96,22 @@ public class MooSuite extends Problem implements SimpleProblemForm
             problemType = PROB_ZDT4;
         else if ( wp.compareTo( P_ZDT6) == 0 )
             problemType = PROB_ZDT6;
-        else if( wp.compareTo( P_SPHERE) == 0 || wp.compareTo( P_SCH) == 0 )
+        else if ( wp.compareTo( P_FON) == 0 )
+            problemType = PROB_FON;
+        else if ( wp.compareTo( P_POL) == 0 )
+            problemType = PROB_POL;
+        else if ( wp.compareTo( P_QV) == 0 )
+            problemType = PROB_QV;
+        else if ( wp.compareTo( P_KUR) == 0 )
+            problemType = PROB_KUR;
+        else if( wp.compareTo( P_SPHERE) == 0)
             problemType = PROB_SPHERE;         
+        else if( wp.compareTo( P_F2) == 0)
+            problemType = PROB_F2;
+        else if( wp.compareTo( P_F3) == 0)
+            problemType = PROB_F3;
+        else if( wp.compareTo( P_SCH) == 0 || wp.compareTo( P_F1) == 0 )
+            problemType = PROB_SCH;         
         else state.output.fatal(
             "Invalid value for parameter, or parameter not found.\n" +
             "Acceptable values are:\n" +
@@ -99,13 +120,22 @@ public class MooSuite extends Problem implements SimpleProblemForm
             "  " + P_ZDT3 + "\n" +
             "  " + P_ZDT4 + "\n" +
             "  " + P_ZDT6 + "\n" +
-            "  " + P_SPHERE + "(or " + P_SCH + ")\n",
+            "  " + P_POL + "\n" +
+            "  " + P_FON + "\n" +
+            "  " + P_KUR + "\n" +
+            "  " + P_SPHERE + "\n" +
+            "  " + P_SCH + "(or " + P_F1 + ")\n"+
+            "  " + P_F2 + "\n",
             base.push( P_WHICH_PROBLEM ) );
         }
-	
+    private static final double TWO_PI = Math.PI*2;//QV uses it.
     private static final double TEN_PI = Math.PI*10;//ZDT3 uses it.
 	private static final double FOUR_PI = Math.PI*4;//ZDT4 uses it.
-	
+	private static final double SIX_PI = Math.PI*6;//ZDT6 uses it.
+	private static final double ONE_OVER_SQRT_3 = 1d/Math.sqrt(3);//FON uses it.
+	private static final double A1 = 0.5*Math.sin(1) - 2*Math.cos(1) +    Math.sin(2)- 1.5*Math.cos(2);//POL uses it
+	private static final double A2 = 1.5*Math.sin(1) -   Math.cos(1) + 2* Math.sin(2)- 0.5*Math.cos(2);//POL uses it
+
     public void evaluate(final EvolutionState state,
         final Individual ind,
         final int subpopulation,
@@ -169,6 +199,14 @@ public class MooSuite extends Problem implements SimpleProblemForm
         		fitnesses[1] = (float)(g*h);
         		break;                
             case PROB_ZDT6:
+        		f = 1 - (Math.exp(-4 * genome[0]) * Math.pow(Math.sin(SIX_PI * genome[0]), 6));
+        		fitnesses[0] = (float)f;
+        		sum = 0;
+        		for (int i = 1; i < numDecisionVars; ++i)
+        			sum += genome[i];
+        		g = 1d + 9 * Math.pow(sum / (numDecisionVars - 1), 0.25);
+        		h = 1d - Math.pow(f / g, 2);
+        		fitnesses[1] = (float) (g * h);
         		break; 
             case PROB_SPHERE:
         		int numObjectives = fitnesses.length;
@@ -181,6 +219,87 @@ public class MooSuite extends Problem implements SimpleProblemForm
         			fitnesses[j] = (float)sum;
         		}
         		break;
+            case PROB_SCH:
+            	if(numDecisionVars!=1) throw new RuntimeException("SCH needs exactly 1 decision variable (gene).");
+        		double x = genome[0];
+        		fitnesses[0]=(float)(x*x);
+        		fitnesses[1]=(float)((x-2)*(x-2));
+        		break;
+            case PROB_F2:
+            	if(numDecisionVars!=1) throw new RuntimeException("F2 needs exactly 1 decision variable (gene).");
+        		x = genome[0];
+        		fitnesses[0]=(float)( x<=1? -x: (x<=3? x-2:(x<=4? 4-x: x-4)));
+        		fitnesses[1]=(float)((x-5)*(x-5));
+        		break;
+            case PROB_F3:
+            	if(numDecisionVars!=2) throw new RuntimeException("F3 needs exactly 2 decision variable (gene).");
+        		double x1 = genome[0];
+        		double x2 = genome[1];
+        		fitnesses[0]=(float)((x1-2)*(x1-2)+(x2-1)*(x2-1)+2);
+        		fitnesses[1]=(float)(9*x1-(x2-1)*(x2-1));
+        		break;
+        	case PROB_FON:
+            	if(numDecisionVars!=3) throw new RuntimeException("FON needs exactly 3 decision variables (genes).");
+            	double sum1 = 0, sum2=0;
+        		for(int i = 1; i< numDecisionVars; ++i)
+        		{
+        			double xi = genome[i];
+        			double d = xi-ONE_OVER_SQRT_3;
+        			double s = xi+ONE_OVER_SQRT_3;
+        			sum1+=d*d;
+        			sum2+=s*s;
+        		}
+        		fitnesses[0] = (float)Math.exp(-sum1);
+        		fitnesses[1] = (float)Math.exp(-sum2);
+            	break;
+            case PROB_POL:
+            	if(numDecisionVars!=2) throw new RuntimeException("POL needs exactly 2 decision variables (genes).");
+            	x1= genome[0];
+            	x2 = genome[1];
+            	double b1 = 0.5*Math.sin(x1) - 2*Math.cos(x1) +    Math.sin(x2)- 1.5*Math.cos(x2);
+            	double b2 = 1.5*Math.sin(x1) -   Math.cos(x1) + 2* Math.sin(x2)- 0.5*Math.cos(x2);
+            	fitnesses[0] = (float)(1+(A1-b1)*(A1-b1)+(A2-b2)*(A2-b2));
+            	fitnesses[1] = (float)((x1+3)*(x1+3)+(x2+1)*(x2+1));
+            	break;
+            case PROB_QV:
+            	sum=0;
+            	for(int i=0;i<numDecisionVars;i++)
+            	{
+            		double xi=genome[i];
+            		sum+=xi*xi-10*Math.cos(TWO_PI*xi)+10;
+            	}
+            	fitnesses[0] = (float)Math.pow(sum/numDecisionVars, 0.25);
+            	sum=0;
+            	for(int i=0;i<numDecisionVars;i++)
+            	{
+            		double xi=genome[i]-1.5;
+            		sum+=xi*xi-10*Math.cos(TWO_PI*xi)+10;
+            	}
+            	fitnesses[1] = (float)Math.pow(sum/numDecisionVars, 0.25);
+            	break;
+            case PROB_KUR:
+        		double nextSquared, thisSquared;
+        		thisSquared = genome[0]*genome[0];
+        		sum=0;
+        		for(int i = 0; i< numDecisionVars-1; ++i)
+        		{
+        			nextSquared = genome[i+1]*genome[i+1];
+        			sum += 1d-Math.exp(-0.2*Math.sqrt(thisSquared + nextSquared));
+        			thisSquared = nextSquared;
+        		}
+        		fitnesses[1] = (float)sum;
+        		sum= 0;
+        		for(int i = 0; i< numDecisionVars; ++i)
+        		{
+        			double sin_xi = Math.sin(genome[i]);
+        			
+        			double t1 = Math.pow(Math.abs(genome[i]), .8);
+        			double t2 = 5*sin_xi*sin_xi*sin_xi;
+        			
+        			sum +=t1+t2+ 3.5828;
+        		}
+        		fitnesses[0] = (float)sum;
+            	break;
 
             default:
                 state.output.fatal( "ec.app.ecsuite.ECSuite has an invalid problem -- how on earth did that happen?" );
