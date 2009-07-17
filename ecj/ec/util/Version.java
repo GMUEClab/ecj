19b583517d271ba6b46e8aec29f68b7dede89fe4
feijai <feijai@1c647de6-f618-7a77-627f-ce647b82753d>
2009-07-17 19:13:47 +0000

    Yo Issue number: Obtained from: Submitted by: Reviewed by:

--- ecj/ec/util/Version.java
+++ ecj/ec/util/Version.java
@@ -31,7 +31,7 @@ public class Version
     public static final String author = "Sean Luke";
     public static final String contributors = "L. Panait, G. Balan, S. Paus, Z. Skolicki, R. Kicinger, E. Popovici,";
     public static final String contributors2 = "K. Sullivan, J. Harrison, J. Bassett, R. Hubley, A. Desai, A. Chircop,";
-    public static final String contributors3 = "J. Compton, W. Haddon, S. Donnelly, B. Jamil, J. O'Beirne";
+    public static final String contributors3 = "J. Compton, W. Haddon, S. Donnelly, B. Jamil, and J. O'Beirne";
     public static final String authorEmail0 = "ecj-help";
     public static final String authorEmail1 = "cs.gmu.edu";
     public static final String authorEmail2 = "(better: join ECJ-INTEREST at URL above)";
