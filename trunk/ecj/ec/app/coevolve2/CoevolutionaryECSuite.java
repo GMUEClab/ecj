206aedc4b8c5163590445b446c6fe693802ce0ca
sean@cs.gmu.edu <sean@cs.gmu.edu@1c647de6-f618-7a77-627f-ce647b82753d>
2010-12-05 19:35:24 +0000

    yo

--- trunk/ecj/ec/app/coevolve2/CoevolutionaryECSuite.java
+++ trunk/ecj/ec/app/coevolve2/CoevolutionaryECSuite.java
@@ -36,53 +36,53 @@ public class CoevolutionaryECSuite extends ECSuite implements GroupedProblemForm
         int[] subpops,
         final int threadnum)
         {
-		if (ind.length == 0)
-			state.output.fatal("Number of individuals provided to CoevolutionaryECSuite is 0!");
-		if (ind.length == 1)
-			state.output.warnOnce("Coevolution used, but number of individuals provided to CoevolutionaryECSuite is 1.");
-		
-		int size = 0;
-		for(int i = 0 ; i < ind.length; i++)
-			if ( ! ( ind[i] instanceof CoevolutionaryDoubleVectorIndividual ) )
-				state.output.error( "Individual " + i + "in coevolution is not a CoevolutionaryDoubleVectorIndividual." );
-			else
-				{
-				CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
-				size += coind.genome.length;
-				}
-		state.output.exitIfErrors();
-		
-		// concatenate all the arrays
-		double[] vals = new double[size];
-		int pos = 0;
-		for(int i = 0 ; i < ind.length; i++)
-			{
-			System.err.println("-->" + i);
-			CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
-			System.err.println(coind.genome.length);
-			System.arraycopy(coind.genome, 0, vals, pos, coind.genome.length);
-			pos += coind.genome.length;
-			}
+        if (ind.length == 0)
+            state.output.fatal("Number of individuals provided to CoevolutionaryECSuite is 0!");
+        if (ind.length == 1)
+            state.output.warnOnce("Coevolution used, but number of individuals provided to CoevolutionaryECSuite is 1.");
+                
+        int size = 0;
+        for(int i = 0 ; i < ind.length; i++)
+            if ( ! ( ind[i] instanceof CoevolutionaryDoubleVectorIndividual ) )
+                state.output.error( "Individual " + i + "in coevolution is not a CoevolutionaryDoubleVectorIndividual." );
+            else
+                {
+                CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
+                size += coind.genome.length;
+                }
+        state.output.exitIfErrors();
+                
+        // concatenate all the arrays
+        double[] vals = new double[size];
+        int pos = 0;
+        for(int i = 0 ; i < ind.length; i++)
+            {
+            System.err.println("-->" + i);
+            CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
+            System.err.println(coind.genome.length);
+            System.arraycopy(coind.genome, 0, vals, pos, coind.genome.length);
+            pos += coind.genome.length;
+            }
 
-		double fit = (function(state, problemType, vals, threadnum));
-		boolean isOptimal = isOptimal(problemType, fit);
+        double fit = (function(state, problemType, vals, threadnum));
+        boolean isOptimal = isOptimal(problemType, fit);
 
-		for(int i = 0 ; i < ind.length; i++)
-			{
-			CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
-			if (updateFitness[i])
-				{
-				if ( fit > coind.fitness.fitness() )
-					{
-					((SimpleFitness)(coind.fitness)).setFitness( state, (float) fit, isOptimal );
-					coind.context = new CoevolutionaryDoubleVectorIndividual[ind.length];
-					for(int j = 0; j < ind.length; j++)
-						{
-						if (i != j)
-						coind.context[j] = (CoevolutionaryDoubleVectorIndividual)(ind[j]);
-						}
-					}
-				}
-			}
+        for(int i = 0 ; i < ind.length; i++)
+            {
+            CoevolutionaryDoubleVectorIndividual coind = (CoevolutionaryDoubleVectorIndividual)(ind[i]);
+            if (updateFitness[i])
+                {
+                if ( fit > coind.fitness.fitness() )
+                    {
+                    ((SimpleFitness)(coind.fitness)).setFitness( state, (float) fit, isOptimal );
+                    coind.context = new CoevolutionaryDoubleVectorIndividual[ind.length];
+                    for(int j = 0; j < ind.length; j++)
+                        {
+                        if (i != j)
+                            coind.context[j] = (CoevolutionaryDoubleVectorIndividual)(ind[j]);
+                        }
+                    }
+                }
+            }
         }
     }
