1db3b07031320525f829b1b1d77f860a48c2800a
sean <sean@cs.gmu.edu>
2012-04-23 22:06:21 +0000

    yo

---
+++ ecj/ec/app/regression/func/Neg.java
@@ -0,0 +1,50 @@
+/*
+  Copyright 2012 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+
+package ec.app.regression.func;
+import ec.*;
+import ec.app.regression.*;
+import ec.gp.*;
+import ec.util.*;
+
+/* 
+ * Neg.java
+ * 
+ * Created: Mon Apr 23 17:15:35 EDT 2012
+ * By: Sean Luke
+
+ <p>This function appears in the Keijzer function set, and is (0 - x)
+ <p>M. Keijzer. Improving Symbolic Regression with Interval Arithmetic and Linear Scaling. In <i>Proc. EuroGP.</i> 2003.
+ */
+
+/**
+ * @author Sean Luke
+ * @version 1.0 
+ */
+
+public class Neg extends GPNode
+    {
+    public String toString() { return "0-"; }
+
+    public int expectedChildren() { return 1; }
+
+    public void eval(final EvolutionState state,
+        final int thread,
+        final GPData input,
+        final ADFStack stack,
+        final GPIndividual individual,
+        final Problem problem)
+        {
+        RegressionData rd = ((RegressionData)(input));
+
+        children[0].eval(state,thread,input,stack,individual,problem);
+        rd.x = 0.0 - rd.x;
+        }
+    }
+
+
+
---
+++ ecj/ec/app/regression/func/Sqrt.java
@@ -0,0 +1,52 @@
+/*
+  Copyright 2012 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+
+package ec.app.regression.func;
+import ec.*;
+import ec.app.regression.*;
+import ec.gp.*;
+import ec.util.*;
+
+/* 
+ * Sqrt.java
+ * 
+ * Created: Mon Apr 23 17:15:35 EDT 2012
+ * By: Sean Luke
+
+ <p>This function appears in the Korns and Keijzer function sets, and is sqrt(x)
+ <p>M. F. Korns. Accuracy in Symbolic Regression. In <i>Proc. GPTP.</i> 2011.
+ <p>M. Keijzer. Improving Symbolic Regression with Interval Arithmetic and Linear Scaling. In <i>Proc. EuroGP.</i> 2003.
+
+ */
+
+/**
+ * @author Sean Luke
+ * @version 1.0 
+ */
+
+public class Sqrt extends GPNode
+    {
+    public String toString() { return "sqrt"; }
+
+    public int expectedChildren() { return 1; }
+
+    public void eval(final EvolutionState state,
+        final int thread,
+        final GPData input,
+        final ADFStack stack,
+        final GPIndividual individual,
+        final Problem problem)
+        {
+        RegressionData rd = ((RegressionData)(input));
+
+        children[0].eval(state,thread,input,stack,individual,problem);
+        rd.x = Math.sqrt(rd.x);
+        }
+    }
+
+
+
