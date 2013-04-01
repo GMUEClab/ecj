14cc41be1414e334c13a4bb55dd7a66c7a2a8241
sean@cs.gmu.edu <sean@cs.gmu.edu@1c647de6-f618-7a77-627f-ce647b82753d>
2013-04-01 19:03:05 +0000

    yo

--- trunk/ecj/ec/simple/SimpleEvaluator.java
+++ trunk/ecj/ec/simple/SimpleEvaluator.java
@@ -23,7 +23,11 @@ import ec.util.*;
  * each evaluating thread.  The Problem must implement SimpleProblemForm.
  *
  * @author Sean Luke
- * @version 1.0 
+ * @version 2.0 
+ *
+ * Thanks to Ralf Buschermohle <lobequadrat@googlemail.com> for early versions
+ * of code which led to this version.
+ *
  */
 
 public class SimpleEvaluator extends Evaluator
@@ -31,11 +35,14 @@ public class SimpleEvaluator extends Evaluator
     public static final String P_CLONE_PROBLEM = "clone-problem";
     public static final String P_NUM_TESTS = "num-tests";
     public static final String P_MERGE = "merge";
-    
+
     public static final String V_MEAN = "mean";
     public static final String V_MEDIAN = "median";
     public static final String V_BEST = "best";
-    
+        
+    public static final String P_CHUNK_SIZE = "chunk-size";
+    public static final String V_AUTO = "auto";
+
     public static final int MERGE_MEAN = 0;
     public static final int MERGE_MEDIAN = 1;
     public static final int MERGE_BEST = 2;
@@ -43,6 +50,14 @@ public class SimpleEvaluator extends Evaluator
     public int numTests = 1;
     public int mergeForm = MERGE_MEAN;
     public boolean cloneProblem;
+
+    Object[] lock = new Object[0];          // Arrays are serializable
+    int individualCounter = 0;
+    int subPopCounter = 0;
+    int chunkSize;  // a value >= 1, or C_AUTO
+    public static final int C_AUTO = 0;
+        
+    public ThreadPool pool = new ThreadPool();
 
     // checks to make sure that the Problem implements SimpleProblemForm
     public void setup(final EvolutionState state, final Parameter base)
@@ -72,13 +87,29 @@ public class SimpleEvaluator extends Evaluator
             else
                 state.output.fatal("Bad merge method: " + m, base.push(P_NUM_TESTS), null);
             }
-        }
+                
+        if (!state.parameters.exists(base.push(P_CHUNK_SIZE), null))
+            {
+            chunkSize = C_AUTO;
+            }
+        else if (state.parameters.getString(base.push(P_CHUNK_SIZE), null).equalsIgnoreCase(V_AUTO))
+            {
+            chunkSize = C_AUTO;
+            }
+        else
+            {
+            chunkSize = (state.parameters.getInt(base.push(P_CHUNK_SIZE), null, 1));
+            if (chunkSize == 0)  // uh oh
+                state.output.fatal("Chunk Size must be either an integer >= 1 or 'auto'", base.push(P_CHUNK_SIZE), null);
+            }
+        } 
 
     Population oldpop = null;
+    // replace the population with one that has some N copies of the original individuals
     void expand(EvolutionState state)
         {
         Population pop = (Population)(state.population.emptyClone());
-        
+
         // populate with clones
         for(int i = 0; i < pop.subpops.length; i++)
             {
@@ -92,18 +123,20 @@ public class SimpleEvaluator extends Evaluator
                     }
                 }
             }
-        
+
         // swap
         Population oldpop = state.population;
         state.population = pop;
         }
-                
+
+    // Take the N copies of the original individuals and fold their fitnesses back into the original
+    // individuals, replacing them with the original individuals in the process.  See expand(...)
     void contract(EvolutionState state)
         {
         // swap back
         Population pop = state.population;
         state.population = oldpop;
-        
+
         // merge fitnesses again
         for(int i = 0; i < pop.subpops.length; i++)
             {
@@ -114,7 +147,7 @@ public class SimpleEvaluator extends Evaluator
                     {
                     fits[k] = pop.subpops[i].individuals[numTests * j + k].fitness;
                     }
-                                
+
                 if (mergeForm == MERGE_MEAN)
                     {
                     state.population.subpops[i].individuals[j].fitness.setToMeanOf(state, fits);
@@ -127,13 +160,12 @@ public class SimpleEvaluator extends Evaluator
                     {
                     state.population.subpops[i].individuals[j].fitness.setToBestOf(state, fits);
                     }
-                                
+
                 state.population.subpops[i].individuals[j].evaluated = true;
                 }
             }
         }
-
-
+        
     /** A simple evaluator that doesn't do any coevolutionary
         evaluation.  Basically it applies evaluation pipelines,
         one per thread, to various subchunks of a new population. */
@@ -141,94 +173,67 @@ public class SimpleEvaluator extends Evaluator
         {
         if (numTests > 1)
             expand(state);
-                
-        if (state.evalthreads==1)
+            
+        // reset counters.  Only used in multithreading
+        individualCounter = 0;
+        subPopCounter = 0;
+
+        // start up if single-threaded?
+        if (state.evalthreads == 1)
             {
-            // a minor bit of optimization
-            int numinds[] = new int[state.population.subpops.length];
-            int from[] = new int[state.population.subpops.length];
-            for(int i = 0; i < state.population.subpops.length; i++)
-                { numinds[i] = state.population.subpops[i].individuals.length; from[i] = 0; }
+            int[] numinds = new int[state.population.subpops.length];
+            int[] from = new int[numinds.length];
+                        
+            for(int i = 0; i < numinds.length; i++)
+                {
+                numinds[i] =  state.population.subpops[i].individuals.length;
+                from[i] = 0;
+                }
+                                
+            SimpleProblemForm prob = null;
             if (cloneProblem)
-                evalPopChunk(state,numinds,from,0,(SimpleProblemForm)(p_problem.clone()));
-            else
-                evalPopChunk(state,numinds,from,0,(SimpleProblemForm)(p_problem));
+                prob = (SimpleProblemForm)(p_problem.clone());
+            else 
+                prob = (SimpleProblemForm)(p_problem);  // just use the prototype
+            evalPopChunk(state, numinds, from, 0, prob);
             }
         else
             {
-            // how many threads do we really need?  No more than the maximum number of individuals in any subpopulation
-            int numThreads = 0;
-            for(int x = 0; x < state.population.subpops.length; x++)
-                numThreads = Math.max(numThreads, state.population.subpops[x].individuals.length);
-            numThreads = Math.min(numThreads, state.evalthreads);
-            if (numThreads < state.breedthreads)
-                state.output.warnOnce("Largest subpopulation size (" + numThreads +") is smaller than number of evalthreads (" + state.breedthreads +
-                    "), so fewer evalthreads will be created.");
-            
-            int numinds[][] = 
-                new int[numThreads][state.population.subpops.length];
-            int from[][] = 
-                new int[numThreads][state.population.subpops.length];
-        
-            for(int x=0;x<state.population.subpops.length;x++)
+            Thread[] threads = new Thread[state.evalthreads];
+            for(int i = 0; i < threads.length; i++)
                 {
-                // we will have some extra individuals.  We distribute these among the early subpopulations
-                int individualsPerThread = state.population.subpops[x].individuals.length / numThreads;  // integer division
-                int slop = state.population.subpops[x].individuals.length - numThreads * individualsPerThread;
-                int currentFrom = 0;
-                                
-                for(int y=0;y<numThreads;y++)
-                    {
-                    if (slop > 0)
-                        {
-                        numinds[y][x] = individualsPerThread + 1;
-                        slop--;
-                        }
-                    else
-                        numinds[y][x] = individualsPerThread;
-                    
-                    if (numinds[y][x] == 0)
-                        {
-                        state.output.warnOnce("More threads exist than can be used to evaluate some subpopulations (first example: subpopulation " + x + ")");
-                        }
-
-                    from[y][x] = currentFrom;
-                    currentFrom += numinds[y][x];
-                    }
+                SimpleEvaluatorThreadCG run = new SimpleEvaluatorThreadCG();
+                run.threadnum = i;
+                run.state = state;
+                run.prob = (SimpleProblemForm)(p_problem.clone());
+                threads[i] = pool.startThread("ECJ Evaluation Thread " + i, run);
                 }
-
-            Thread[] t = new Thread[numThreads];
-            
-            // start up the threads
-            for(int y=0;y<numThreads;y++)
-                {
-                SimpleEvaluatorThread r = new SimpleEvaluatorThread();
-                r.threadnum = y;
-                r.numinds = numinds[y];
-                r.from = from[y];
-                r.me = this;
-                r.state = state;
-                r.p = (SimpleProblemForm)(p_problem.clone()); // should ignore cloneProblem parameter here 
-                t[y] = new Thread(r);
-                t[y].start();
-                }
-
-            // gather the threads
-            for(int y=0;y<numThreads;y++) 
-                try 
-                    { 
-                    t[y].join(); 
-                    }
-                catch(InterruptedException e)
-                    {
-                    state.output.fatal("Whoa! The main evaluation thread got interrupted!  Dying...");
-                    }
-
+                        
+            // join
+            for(int i = 0; i < threads.length; i++)
+                pool.joinAndReturn(threads[i]);
             }
-            
+
         if (numTests > 1)
             contract(state);
         }
+
+
+    /** The SimpleEvaluator determines that a run is complete by asking
+        each individual in each population if he's optimal; if he 
+        finds an individual somewhere that's optimal,
+        he signals that the run is complete. */
+    public boolean runComplete(final EvolutionState state)
+        {
+        for(int x = 0;x<state.population.subpops.length;x++)
+            for(int y=0;y<state.population.subpops[x].individuals.length;y++)
+                if (state.population.subpops[x].
+                    individuals[y].fitness.isIdealFitness())
+                    return true;
+        return false;
+        }
+
+
 
     /** A private helper function for evaluatePopulation which evaluates a chunk
         of individuals in a subpopulation for a given thread.
@@ -256,31 +261,87 @@ public class SimpleEvaluator extends Evaluator
                         
         ((ec.Problem)p).finishEvaluating(state,threadnum);
         }
-    
-    /** The SimpleEvaluator determines that a run is complete by asking
-        each individual in each population if he's optimal; if he 
-        finds an individual somewhere that's optimal,
-        he signals that the run is complete. */
-    public boolean runComplete(final EvolutionState state)
-        {
-        for(int x = 0;x<state.population.subpops.length;x++)
-            for(int y=0;y<state.population.subpops[x].individuals.length;y++)
-                if (state.population.subpops[x].
-                    individuals[y].fitness.isIdealFitness())
-                    return true;
-        return false;
-        }
-    }
 
-/** A private helper class for implementing multithreaded evaluation */
-class SimpleEvaluatorThread implements Runnable
-    {
-    public int[] numinds;
-    public int[] from;
-    public SimpleEvaluator me;
-    public EvolutionState state;
-    public int threadnum;
-    public SimpleProblemForm p;
-    public synchronized void run() 
-        { me.evalPopChunk(state,numinds,from,threadnum,p); }
+
+	// computes the chunk size if 'auto' is set.  This may be different depending on the subpopulation,
+	// which is backward-compatible with previous ECJ approaches.
+    int computeChunkSizeForSubpopulation(EvolutionState state, int subpop, int threadnum)
+        {
+        int numThreads = state.evalthreads;
+                
+        // we will have some extra individuals.  We distribute these among the early subpopulations
+        int individualsPerThread = state.population.subpops[subpop].individuals.length / numThreads;  // integer division
+        int slop = state.population.subpops[subpop].individuals.length - numThreads * individualsPerThread;
+                
+        if (threadnum >= slop) // beyond the slop
+            return individualsPerThread;
+        else return individualsPerThread + 1;
+        }
+
+
+
+/** A helper class for implementing multithreaded evaluation */
+    class SimpleEvaluatorThreadCG implements Runnable
+        {
+        public int threadnum;
+        public EvolutionState state;
+        public SimpleProblemForm prob = null;
+        
+        public void run() 
+            {
+            Subpopulation[] subpops = state.population.subpops;
+
+            int[] numinds = new int[subpops.length];
+            int[] from = new int[subpops.length];
+
+			int count = 1;
+			int start = 0;
+			int subpop = 0;
+
+            while (true)
+                {
+				// We need to grab the information about the next chunk we're responsible for.  This stays longer
+				// in the lock than I'd like :-(
+                synchronized(lock)
+                    {
+                    // has everyone done all the jobs?
+                    if (subPopCounter >= subpops.length) // all done
+                        return;
+                                        
+                    // has everyone finished the jobs for this subpopulation?
+                    if (individualCounter >= subpops[subPopCounter].individuals.length)  // try again, jump to next subpop
+                        {
+                        individualCounter = 0; 
+                        subPopCounter++;
+                                        
+                        // has everyone done all the jobs?  Check again.
+                        if (subPopCounter >= subpops.length) // all done
+                            return;
+                        }
+
+                    start = individualCounter;
+                    subpop = subPopCounter;
+                    count = chunkSize;
+                    if (count == C_AUTO)  // compute automatically for subpopulations
+                        count = computeChunkSizeForSubpopulation(state, subpop, threadnum);
+                    
+                    individualCounter += count;  // it can be way more than we'll actually do, that's fine                    
+                    }
+                
+                // Modify the true count
+                if (count >= subpops[subpop].individuals.length - start)
+                    count = subpops[subpop].individuals.length - start;
+
+                // Load into arrays to reuse evalPopChunk
+                for(int i = 0; i < from.length; i++)
+                    numinds[i] = 0;
+                        
+                numinds[subpop] = count;
+                from[subpop] = start;
+                evalPopChunk(state, numinds, from, threadnum, prob);
+                }
+            }
+        }
+
+
     }
