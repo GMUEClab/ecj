4ee1fabf09789d486c148c4ed5e9de3ddd5a0093
sean@cs.gmu.edu <sean@cs.gmu.edu@1c647de6-f618-7a77-627f-ce647b82753d>
2014-06-07 13:15:22 +0000

    yo

--- trunk/ecj/ec/eval/SlaveMonitor.java
+++ trunk/ecj/ec/eval/SlaveMonitor.java
@@ -202,8 +202,10 @@ public class SlaveMonitor
                         problemPrototype.sendAdditionalData(state, dataOut);
                         dataOut.flush();
                                                 
-                        registerSlave(state, slaveName, slaveSock, dataOut, dataIn);
-                        state.output.systemMessage( "Slave " + slaveName + " connected successfully." );
+                        if (registerSlave(state, slaveName, slaveSock, dataOut, dataIn))
+	                        state.output.systemMessage( "Slave " + slaveName + " connected successfully." );
+	                    else
+	                        state.output.systemMessage( "Slave " + slaveName + " not permitted to connect." );
                         }
                     catch (IOException e) {  }
                     }
@@ -217,20 +219,31 @@ public class SlaveMonitor
     /**
        Registers a new slave with the monitor.  Upon registration, a slave is marked as available for jobs.
     */
-    public void registerSlave( EvolutionState state, String name, Socket socket, DataOutputStream out, DataInputStream in)
+    public boolean registerSlave( EvolutionState state, String name, Socket socket, DataOutputStream out, DataInputStream in)
         {
+        if (isShutdownInProgress())  // no more registrations.  Kill the socket
+        	{
+        	try { out.writeByte(Slave.V_SHUTDOWN); } catch (Exception e) { }  // exception, not IOException, because JZLib throws some array exceptions
+        	try { out.flush(); } catch (Exception e) { }
+        	try { out.close(); } catch (Exception e) { }
+        	try { in.close(); } catch (Exception e) { }
+        	try { socket.close(); } catch (IOException e) { }
+        	return false;
+        	}
+        
         SlaveConnection newSlave = new SlaveConnection( state, name, socket, out, in, this );
         
-        synchronized(availableSlaves)
-            {
-            availableSlaves.addLast(newSlave);
-            notifyMonitor(availableSlaves);
-            }
         synchronized(allSlaves)
             {
-            allSlaves.addLast(newSlave);
-            notifyMonitor(allSlaves);
+	            allSlaves.addLast(newSlave);
+	            notifyMonitor(allSlaves);
             }
+        synchronized(availableSlaves)
+            {
+            	availableSlaves.addLast(newSlave);
+            	notifyMonitor(availableSlaves);
+            }
+        return true;
         }
 
     /**
@@ -240,13 +253,19 @@ public class SlaveMonitor
         {
         synchronized(allSlaves)
             {
-            allSlaves.remove(slave);
-            notifyMonitor(allSlaves);
+            if (allSlaves.contains(slave))  // could have been removed if shutdown is in progress
+				{
+	            allSlaves.remove(slave);
+	            notifyMonitor(allSlaves);
+	            }
             }
         synchronized(availableSlaves)
             {
-            availableSlaves.remove(slave);
-            notifyMonitor(availableSlaves);
+            if (availableSlaves.contains(slave))  // could have been removed if shutdown is in progress
+				{
+	            availableSlaves.remove(slave);
+	            notifyMonitor(availableSlaves);
+	            }
             }
         }
 
