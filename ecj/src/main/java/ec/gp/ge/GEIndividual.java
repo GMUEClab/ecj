09dd5fb70bf89c2d35a2f796a16301f72d575f71
SigmaX <fyedernoggersnodden@gmail.com>
2017-10-13 15:58:31 -0400

    Fix types in some copyright years. Resolves #22.

--- ecj/src/main/java/ec/gp/ge/GEIndividual.java
+++ ecj/src/main/java/ec/gp/ge/GEIndividual.java
@@ -1,5 +1,5 @@
 /*
-  Copyright 20010 by Sean Luke and George Mason University
+  Copyright 2010 by Sean Luke and George Mason University
   Licensed under the Academic Free License version 3.0
   See the file "LICENSE" for more information
 */
--- ecj/src/main/java/ec/util/ThreadPool.java
+++ ecj/src/main/java/ec/util/ThreadPool.java
@@ -1,5 +1,5 @@
 /*
-  Copyright 20123 by Sean Luke and George Mason University
+  Copyright 2013 by Sean Luke and George Mason University
   Licensed under the Academic Free License version 3.0
   See the file "LICENSE" for more information
 */
