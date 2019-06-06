1298b9c76ef1928cd42932d505652a5d46072b35
SigmaX <fyedernoggersnodden@gmail.com>
2019-06-06 12:41:23 -0400

    Write a simple ConstructiveBreeder to use with both GRASP and ACO.

---
+++ ecj/src/main/java/ec/co/ConstructiveBreeder.java
@@ -0,0 +1,52 @@
+/*
+ Copyright 2017 by Sean Luke
+ Licensed under the Academic Free License version 3.0
+ See the file "LICENSE" for more information
+ */
+package ec.co;
+
+import ec.*;
+import ec.co.ConstructiveIndividual;
+import ec.co.ConstructiveProblemForm;
+import ec.util.Parameter;
+
+/**
+ * A simple Breeder that generates a new population by asking each subpopulation's
+ * Species to create new individuals.
+ *
+ * This is used with algorithms like Grasp and ACO, which generate new individuals at
+ * each step (rather than producing them by running an old population through a pipeline).
+ *
+ * @author Eric O. Scott
+ */
+public class ConstructiveBreeder extends Breeder
+{
+
+    @Override
+    public void setup(final EvolutionState state, final Parameter base)
+    {
+        // Do nothing
+    }
+
+    @Override
+    public Population breedPopulation(final EvolutionState state)
+    {
+        assert(state != null);
+
+        final Population newPop = state.population.emptyClone();
+        // For each subpopulation
+        for (int i = 0; i < state.population.subpops.size(); i++)
+        {
+            final Subpopulation oldSubpop = state.population.subpops.get(i);
+            final Species species = state.population.subpops.get(i).species;
+
+            // Execute ants
+            for (int j = 0; j < oldSubpop.individuals.size(); j++)
+            {
+                final Individual newInd = species.newIndividual(state, i);
+                newPop.subpops.get(i).individuals.add(newInd);
+            }
+        }
+        return newPop;
+    }
+}
