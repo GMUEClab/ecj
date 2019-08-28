504657818fb9f902fb5a8c6413f29db2b999e83e
SigmaX <fyedernoggersnodden@gmail.com>
2019-08-27 22:46:10 -0400

    Add standard param files that define Ant System and Ant Colony System algorithms.  Revised the knapsack and TSP examples to use these, making for pretty simple examples of using ACO.

---
+++ ecj/src/main/java/ec/co/ant/ACSLocalUpdateRule.java
@@ -0,0 +1,71 @@
+/*
+  Copyright 2019 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+package ec.co.ant;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Subpopulation;
+import ec.co.Component;
+import ec.co.ConstructiveIndividual;
+import ec.util.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Eric O. Scott
+ */
+public class ACSLocalUpdateRule implements UpdateRule {
+    final public static String P_RHO = "rho";
+    final public static String P_MINIMUM_PHEROMONE = "minimum-pheromone";
+
+    private double rho;
+    private double minimumPheromone;
+
+    public double getMinimumPheromone() { return minimumPheromone; }
+
+    public double getRho() { return rho; }
+
+    @Override
+    public void setup(final EvolutionState state, final Parameter base)
+    {
+        assert(state != null);
+        assert(base != null);
+        rho = state.parameters.getDouble(base.push(P_RHO), null, 0.0);
+        minimumPheromone = state.parameters.getDouble(base.push(P_RHO), null, 0.0);
+        assert(repOK());
+    }
+
+    @Override
+    public void updatePheromones(final EvolutionState state, final PheromoneTable pheromones, final List individuals) {
+        assert(pheromones != null);
+        assert(individuals != null);
+        assert(!individuals.isEmpty());
+
+        for (final Object ind : individuals)
+        {
+            for (final Object oo : (ConstructiveIndividual)ind)
+            {
+                assert(oo instanceof Component);
+                final Component c = (Component) oo;
+
+                final double oldPheromone = pheromones.get(state, c, 0); // Using thread 0 because we are in a single-threaded function
+                pheromones.set(c, (1-rho)*oldPheromone + rho*minimumPheromone);
+            }
+        }
+
+        assert(repOK());
+    }
+
+    public final boolean repOK()
+    {
+        return P_RHO != null
+                && !P_RHO.isEmpty()
+                && !Double.isInfinite(rho)
+                && !Double.isNaN(rho)
+                && rho >= 0.0;
+    }
+}
