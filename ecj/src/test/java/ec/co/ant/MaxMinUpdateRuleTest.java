23e86cbbaddb662acbc43446fd3ae5bac737f05e
JHilty <javierahilty@gmail.com>
2020-05-18 21:37:21 -0400

    MaxMinAnt manual update initial

---
+++ ecj/src/test/java/ec/co/ant/MaxMinUpdateRuleTest.java
@@ -0,0 +1,148 @@
+/*
+  Copyright 2019 by Sean Luke
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+package ec.co.ant;
+
+import ec.EvolutionState;
+import ec.Evolve;
+import ec.Individual;
+import ec.Subpopulation;
+import ec.app.knapsack.KnapsackProblem;
+import ec.co.Component;
+import ec.co.ConstructiveIndividual;
+import ec.co.ant.MaxMinUpdateRule.BestStrategy;
+import ec.simple.SimpleEvaluator;
+import ec.simple.SimpleEvolutionState;
+import ec.simple.SimpleFitness;
+import ec.util.MersenneTwisterFast;
+import ec.util.Parameter;
+import ec.util.ParameterDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Eric O. Scott
+ */
+public class MaxMinUpdateRuleTest {
+    private final static Parameter BASE = new Parameter("base");
+    private final static Parameter PROBLEM_BASE = new Parameter("prob");
+    private final static Parameter PHEROMONES_BASE = new Parameter("pheromones");
+    private EvolutionState state;
+    private ParameterDatabase params;
+    private SimplePheromoneTable pheromones;
+    private KnapsackProblem problem;
+
+    public MaxMinUpdateRuleTest() {
+    }
+
+    @Before
+    public void setUp()
+    {
+        params = new ParameterDatabase();
+        params.set(PROBLEM_BASE.push(KnapsackProblem.P_SIZES), "1 2 3 4 5 6 7 8 9 10");
+        params.set(PROBLEM_BASE.push(KnapsackProblem.P_VALUES), "1 2 3 4 5 5 4 3 2 1");
+        params.set(PROBLEM_BASE.push(KnapsackProblem.P_KNAPSACK_SIZE), "15");
+        params.set(PHEROMONES_BASE.push(SimplePheromoneTable.P_INITIALIZE_WITH_NOISE), "false");
+        params.set(PHEROMONES_BASE.push(SimplePheromoneTable.P_INITIAL_CONCENTRATION), "100.0");
+        params.set(BASE.push(MaxMinUpdateRule.P_BEST_STRATEGY), BestStrategy.GLOBAL_BEST.toString());
+        params.set(BASE.push(MaxMinUpdateRule.P_RHO), "0.3");
+        params.set(BASE.push(MaxMinUpdateRule.P_MAX),"100");
+
+        params.set(BASE.push(MaxMinUpdateRule.P_DECAY_RATE), "0.5");
+
+        state = new SimpleEvolutionState();
+        state.parameters = params;
+        state.output = Evolve.buildOutput();
+        state.output.getLog(0).silent = true;
+        state.output.getLog(1).silent = true;
+        state.output.setThrowsErrors(true);
+        state.random = new MersenneTwisterFast[] { new MersenneTwisterFast() };
+        state.evaluator = new SimpleEvaluator();
+        problem = new KnapsackProblem();
+        problem.setup(state, PROBLEM_BASE);
+        state.evaluator.p_problem = problem;
+        pheromones = new SimplePheromoneTable();
+        pheromones.setup(state, PHEROMONES_BASE);
+
+    }
+
+    /**
+     * When we call setup(), the bestStrategy and rho attributes should be set
+     * from the ParameterDatabase.
+     */
+    @Test
+    public void testSetup1() {
+        MaxMinUpdateRule instance = new MaxMinUpdateRule();
+        instance.setup(state, BASE);
+
+        assertEquals(instance.getBestStrategy(), BestStrategy.GLOBAL_BEST);
+        assertEquals(instance.getRho(), 0.3, 0.000001);
+        assertTrue(instance.repOK());
+    }
+
+    /**
+     * When we call setup(), the bestStrategy and rho attributes should be set
+     * from the ParameterDatabase.
+     */
+    @Test
+    public void testSetup2() {
+        params.set(BASE.push(MaxMinUpdateRule.P_BEST_STRATEGY), BestStrategy.ITERATION_BEST.toString());
+        MaxMinUpdateRule instance = new MaxMinUpdateRule();
+        instance.setup(state, BASE);
+
+        assertEquals(instance.getBestStrategy(), BestStrategy.ITERATION_BEST);
+        assertEquals(instance.getRho(), 0.3, 0.000001);
+        assertTrue(instance.repOK());
+    }
+
+    /**
+     * Test of updatePheromones method, of class MaxMinUpdateRule.
+     */
+    @Test
+    public void testUpdatePheromones() {
+        MaxMinUpdateRule instance = new MaxMinUpdateRule();
+        instance.setup(state, BASE);
+        final Subpopulation subpop = new Subpopulation();
+        subpop.individuals = new ArrayList<Individual>()
+        {{
+            add(createKnapsackInd(new int[] { 1, 2, 3, 4 }, 500.0));
+            add(createKnapsackInd(new int[] { 1, 2, 3, 4 }, 1000.0));
+            add(createKnapsackInd(new int[] { 0, 3, 6, 8 }, 700.0));
+            add(createKnapsackInd(new int[] { 9, 3, 1, 1}, 800.0));
+        }};
+
+        instance.updatePheromones(state, pheromones, subpop.individuals);
+
+        final List<Component> pComponents = problem.getAllComponents();
+        assertEquals(50.0, pheromones.get(state, pComponents.get(0), 0), 0.00000001);
+        assertEquals(335.0, pheromones.get(state, pComponents.get(1), 0), 0.00000001);
+        assertEquals(335.0, pheromones.get(state, pComponents.get(2), 0), 0.00000001);
+        assertEquals(335.0, pheromones.get(state, pComponents.get(3), 0), 0.00000001);
+        assertEquals(335.0, pheromones.get(state, pComponents.get(4), 0), 0.00000001);
+        assertEquals(50.0, pheromones.get(state, pComponents.get(5), 0), 0.00000001);
+        assertEquals(50.0, pheromones.get(state, pComponents.get(6), 0), 0.00000001);
+        assertEquals(50.0, pheromones.get(state, pComponents.get(7), 0), 0.00000001);
+        assertEquals(50.0, pheromones.get(state, pComponents.get(8), 0), 0.00000001);
+        assertEquals(50.0, pheromones.get(state, pComponents.get(9), 0), 0.00000001);
+    }
+
+    /** @returns a solution to 'problem' with a manually designated fitness value. */
+    private ConstructiveIndividual createKnapsackInd(final int[] components, final double fitness)
+    {
+        assert(components != null);
+        final ConstructiveIndividual ind = new ConstructiveIndividual();
+        final List<Component> pComponents = problem.getAllComponents();
+        for (final int c : components)
+            ind.add(state, pComponents.get(c));
+        ind.fitness = new SimpleFitness();
+        ((SimpleFitness)ind.fitness).setFitness(state, fitness, false);
+        return ind;
+    }
+
+}
