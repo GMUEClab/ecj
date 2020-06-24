381728dae1315aaa6602443185ea4be4e230fb9d
Eric 'Siggy' Scott <ericsiggyscott@gmail.com>
2020-06-24 07:19:10 -0400

    Modify ec.app.command example to use an external script

---
+++ ecj/src/main/resources/ec/app/command/spheroid.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+#
+# An example of a script that encodes a fitness function.
+# 
+#
+fitnesses=$(python -c "import sys; print('\n'.join([str(sum(float(x) for x in line.split(','))) for line in sys.stdin]))")
+echo $fitnesses
